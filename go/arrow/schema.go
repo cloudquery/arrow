ca31eee92780c183a31331b07c89e56993c2ab14
Yevgeny Pats <16490766+yevgenypats@users.noreply.github.com>
2023-04-24 23:31:27 +0300

    fix bug

--- go/arrow/schema.go
+++ go/arrow/schema.go
@@ -197,7 +197,7 @@ func (sc *Schema) Metadata() Metadata            { return sc.meta }
 func (sc *Schema) Fields() []Field               {
 	fields := make([]Field, len(sc.fields))
 	copy(fields, sc.fields)
-	return sc.fields 
+	return fields
 }
 func (sc *Schema) Field(i int) Field             { return sc.fields[i] }
 
